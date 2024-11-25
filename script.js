79104063f2d374bf93f1d846407d4c9ff029f7cc
mary <maryehllen.rosa@escola.pr.gov.br>
2024-11-24 21:29:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-document.querySelectorAll('.faq-question').forEach(item => {
-    item.addEventListener('click', event => {
-        const answer = item.nextElementSibling;
-        answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.faq-question').forEach(item => {
+        item.addEventListener('click', function() {
+            const faqItem = item.parentNode;
+            const answer = item.nextElementSibling;
+            if (faqItem.classList.contains('active')) {
+                faqItem.classList.remove('active');
+                answer.style.display = 'none';
+            } else {
+                faqItem.classList.add('active');
+                answer.style.display = 'block';
+            }
+        });
     });
 });
